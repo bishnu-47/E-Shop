51c82d5c90fb9b279144e930688307e8416fbf5e
bishnu das <das.2312bishnu@gmail.com>
2020-11-26 14:47:24 +0530

    added backend and api routes

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Api route")
+})
+
+app.get("/api", (req, res) =>{
+    res.json(products)
+})
+
+app.get("/api/:id", (req, res) => {
+    product = products.find(p => p._id === req.params.id)
+    res.json(product)
+})
+
+app.listen(5000, ()=>{
+    console.log("Server started at port 5000")
+})
